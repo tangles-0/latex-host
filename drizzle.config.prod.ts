e23fb8b2496ba01ff56df119c2805ad3fed792dc
Tangles <tangles@jetdeck.io>
2026-05-19 18:17:18 +1000

    edit wrong config

--- drizzle.config.prod.ts
+++ drizzle.config.prod.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
 config({
-  path: ".env.production",
+  path: ".env.vercel",
 });
 
 function resolveDatabaseUrl(): string {
